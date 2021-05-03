4ae1b0b892771d0f19c74185463f03f35bf5c3da
gldecurtins <gldecurtins@icloud.com>
2021-05-03 13:18:24 +0200

    Adding cron feature

--- Dockerfile
+++ Dockerfile
@@ -21,12 +21,14 @@ ENV wallet_fingerprint=
 ENV farmer_public_key=""
 ENV pool_public_key=""
 ENV plot_size=32
-ENV maximum_plotters=1
+ENV plots_maximum=1
+ENV plots_cron_enable="false"
+ENV plots_cron_interval="@hourly"
 
 RUN DEBIAN_FRONTEND=noninteractive 
 RUN apt-get update
 RUN apt-get install -y \
-    curl jq ansible tar bash ca-certificates git openssl unzip wget sudo acl build-essential apt nfs-common vim \
+    curl jq ansible tar bash ca-certificates git openssl unzip wget sudo acl build-essential apt nfs-common vim cron \
     python3.8 python3.8-dev python3.8-venv python3.8-distutils python-is-python3
 
 RUN git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules /chia-blockchain
--- create_plot.sh
+++ create_plot.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-running_plotters=`ps -ef | grep "plots create" | grep -v "grep" | wc -l`
+plots_creating=`ps -ef | grep "plots create" | grep -v "grep" | wc -l`
 
-# maximum_plotters is set through Docker environment variables
-if [[ ${running_plotters} -ge ${maximum_plotters} ]];then
-	echo ${running_plotters} plots creating
+# plots_maximum is set through Docker environment variables
+if [[ ${plots_creating} -ge ${plots_maximum} ]];then
+	echo ${plots_creating} plots creating
 else
 	plot_date=`date +%Y-%m-%d-%H-%M`
 	plotting_directory=/plotting/plot-k${plot_size}-${plot_date}
 	mkdir ${plotting_directory}
-
     
     cd /chia-blockchain
     . ./activate
 
-    # wallet_fingerprint, farmer_public_key, pool_public_key and plot_size are set through Docker environment variables
+	# wallet_fingerprint, farmer_public_key, pool_public_key and plot_size are set through Docker environment variables
 	chia plots create -a ${wallet_fingerprint} -f ${farmer_public_key} -p ${pool_public_key} \
     -k ${plot_size} -t ${plotting_directory} -d /plots &> /plotting/plot-k${plot_size}-${plot_date}.txt &
 fi
