fa169d23ae01bd997b2e892933a313f0d8aff176
Gian Luca Decurtins <gldecurtins@icloud.com>
2021-05-08 00:40:49 +0200

    Adjust docker build script to support the new buildx platform

--- push_container.sh
+++ push_container.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+docker buildx create --name mybuilder
+docker buildx use mybuilder
 docker buildx build --platform linux/amd64,linux/arm64 -t gldecurtins/chia-docker:latest --push .
