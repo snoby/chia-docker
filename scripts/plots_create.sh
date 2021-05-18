ea5666c644c92b834270db2e48ec673dc2d0049d
Gian Luca Decurtins <gldecurtins@icloud.com>
2021-05-18 14:40:11 +0200

    Error handling and messages

--- scripts/plots_create.sh
+++ scripts/plots_create.sh
@@ -2,20 +2,65 @@
 
 # Usage: create_plot.sh <tmp_dir>
 #   Script will use system value plots_tmp_dir in case tmp_dir not passed
-#   as first command line argument.
+#   as first command line argument. Second argument would override the final_dir.
 #   There is upload_plot.sh which can be used to upload finished plots to
 #   the farmer in case plotter and farmer run on different systems.
 
 [ -n "$1" ] && plots_tmp_dir=$1
+[ -n "$2" ] && plots_final_dir=$2
 
-plots_creating=`ps -ef | grep "plots create" | grep "${plots_tmp_dir}" | grep -v "grep" | wc -l`
+process_available () {
+    process_available_return=0
 
-# Allow only 1 plots create process per tmp_dir
-if [[ ${plots_creating} -ge 1 ]]; then
-    echo "plots create running on ${plots_tmp_dir}"
-else
-    chia_update.sh
+    plots_creating=`ps -ef | grep "plots create" | grep "tmp_dir ${plots_tmp_dir}" | grep -v "grep" | wc -l`
+    [[ ${plots_creating} -ge 1 ]] && process_available_return=1
 
+    return ${process_available_return}
+}
+
+enough_space_available () {
+    enough_space_available_return=0
+
+    POSIXLY_CORRECT=true
+    free_space_tmp_dir_512_blocks=`df -P ${plots_tmp_dir} | sed '1d' | awk '{print $4}'`
+    free_space_tmp_dir_gb=`expr ${free_space_tmp_dir_512_blocks} / 2097152`
+    [[ ${plots_size} -eq 32 && ${free_space_tmp_dir_gb} -le 257 ]]  && enough_space_available_return=1
+    [[ ${plots_size} -eq 33 && ${free_space_tmp_dir_gb} -le 550 ]]  && enough_space_available_return=1
+    [[ ${plots_size} -eq 34 && ${free_space_tmp_dir_gb} -le 1118 ]] && enough_space_available_return=1
+    [[ ${plots_size} -eq 35 && ${free_space_tmp_dir_gb} -le 2335 ]] && enough_space_available_return=1
+
+    return ${enough_space_available_return}
+}
+
+ready_to_plot () {
+    script_exit=0
+
+    if [[ ! -d ${plots_tmp_dir} ]]; then
+        echo "ERROR: Directory ${plots_tmp_dir} does not exist."
+        script_exit=1
+        return 1
+    fi
+
+    if [[ ! -w ${plots_tmp_dir} ]]; then
+        echo "ERROR: Directory ${plots_tmp_dir} is not writeable."
+        script_exit=1
+        return 1
+    fi
+
+    if ! process_available; then
+        echo "INFO: Plots create running on ${plots_tmp_dir}."
+        script_exit=0
+        return 1
+    fi
+
+    if ! enough_space_available; then
+        echo "ERROR: Not enough space on ${plots_tmp_dir}."
+        script_exit=1
+        return 1
+    fi
+}
+
+if ready_to_plot; then
     plots_date=`date +%Y-%m-%d-%H-%M`
     plots_id="plot-k${plots_size}-${plots_date}"
 
@@ -24,9 +69,8 @@ else
     cd ${chia_dir}
     . ./activate
 
-    # Create new plot
-    echo "Creating plot into temporary directory ${plots_tmp_dir}/${plots_id}."
-    echo "Final directory ${plots_final_dir}."
+    echo "INFO: Creating plot into temporary directory ${plots_tmp_dir}/${plots_id}."
+    echo "INFO: Final directory ${plots_final_dir}."
     eval ${prevent_sleep} chia plots create \
         --tmp_dir ${plots_tmp_dir}/${plots_id} \
         --final_dir ${plots_final_dir} \
@@ -37,4 +81,7 @@ else
         ${plots_options} > ${plots_tmp_dir}/${plots_id}.txt
     
     rm -rf ${plots_tmp_dir}/${plots_id}
+else
+    echo "INFO: Not triggering plots create."
+    exit ${script_exit}
 fi
--- scripts/plots_upload.sh
+++ scripts/plots_upload.sh
@@ -6,10 +6,10 @@ plots_uploading=`ps -ef | grep "curl --upload-file" | grep "${plots_final_dir}" 
 
 # Allow only 1 upload file process per final_dir
 if [[ ${plots_uploading} -ge 1 ]]; then
-    echo "curl upload-file running on ${plots_final_dir}"
+    echo "INFO: Curl upload-file running on ${plots_final_dir}"
 else
-    echo "Uploading any finished plots from ${plots_final_dir}."
-    echo "Curl target ${plots_curl_target}."
+    echo "INFO: Uploading any finished plots from ${plots_final_dir}."
+    echo "INFO: Curl target ${plots_curl_target}."
     eval ${prevent_sleep} find ${plots_final_dir} \
         -name '*.plot' \
         -type f \
